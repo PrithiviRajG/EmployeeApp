67d4aceb2a09f999ac05b5ea7ebf013c7ff39495
Prithiviraj <prithiviraj1991@gmail.com>
2018-10-05 17:57:56 +0530

    latest update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, 
+  MatToolbarModule, MatGridListModule,
+  MatFormFieldModule, MatSelectModule,
+  MatInputModule, MatDatepickerModule,
+  MatNativeDateModule} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,
+    MatToolbarModule, MatGridListModule,
+    MatFormFieldModule, MatSelectModule,
+    MatInputModule, MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
